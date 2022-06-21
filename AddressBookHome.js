10e4817c3ebf3e7e58f1e84800ed24ea8c3fef4c
softramesh29 <softramesh29@gmail.com>
2022-06-21 17:13:08 +0530

    Added : Ability to delete the contact from the list and update the address book list file added

--- AddressBookHome.js
+++ AddressBookHome.js
@@ -35,4 +35,16 @@ const createInnerHtml = () => {
     document.querySelector('#table-display').innerHTML = innerHtml ;
 }
 
+const remove = (node) => {
+    let addressBookData = addressBookList.find(perData => perData._id == node.id);
+    if(!addressBookData) return;
+    const index = addressBookList   
+                  .map(perData => perData._id)
+                  .indexOf(addressBookData._id);
+    addressBookList.splice(index, 1);
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+    document.querySelector(".contact-count").textContent = addressBookList.length;
+    createInnerHtml();
+}
+
 
